c85a5361b915acc1be70f46ac5ba730818963211
Bhumi Yadav <bhumiy201@gail.com>
2026-08-08 22:22:49 +0530

    Updated consumer logic

--- producer/consumer.py
+++ producer/consumer.py
@@ -3,11 +3,11 @@ import json
 import snowflake.connector
 
 conn=snowflake.connector.connect(
-     user="BHUMI123",
+     user="RATATOUILLE11",
 
-password="gGkAZLfRNK8pRp6",
+password="QWErtyuiop963258741",
 
-account="FIFIAPE-KZ59572",
+account="UUBMYTG-TL42801",
       warehouse="COMPUTE_WH",
       database="ATMOSYNC_DB",
       schema="PUBLIC"
@@ -30,4 +30,18 @@ json.loads(m.decode("utf-8")),
 print("Waiting for messages...\n")
 
 for message in consumer:
-    print(message.value)
+    data=message.value 
+    cusror.execute(""" INSERT INTO RAW_SENSOR_DATA_LIVE (TIMESTAMP,CONTAINER_ID,COMMODITY_TYPE,TEMPERATURE_C,HUMIDITY_PCT,GPS_LAT,READING_ID,ANOMALY)
+    VALUES(%s,%s,%s,%s,%s,%s,%s,%s)""",(
+     data["timestamp"],
+     data["container_id"],
+     data["commodity_type"],
+     data["temperature_c"],
+     data["humidity_pct"],
+     data["gps_lat"],
+     data["reading_id"],
+     data["anomaly"]    
+))
+
+conn.commit()
+print("Inserted:",data)
